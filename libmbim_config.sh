d7402558222c79a4d9b72765f6bf1afe31e43fb8
SunXinzhao <40970110+zixi11@users.noreply.github.com>
2022-08-24 14:56:39 +0800

    Upload Script

---
+++ libmbim_config.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+pip3 install meson
+sudo apt-get install pkg-config gcc make cmake -y
+sudo apt-get install glib-2.0 -y
+sudo apt-get install libgirepository1.0-dev -y
+sudo apt-get install help2man -y
+pip3 install Ninja
